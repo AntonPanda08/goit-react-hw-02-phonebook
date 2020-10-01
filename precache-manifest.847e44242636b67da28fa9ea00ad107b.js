b55ba7c57f281675abce82886d8e1357c635e2a1
AntonPanda08 <roll2001@ukr.net>
2020-10-01 11:34:36 +0300

    Updates

---
+++ precache-manifest.847e44242636b67da28fa9ea00ad107b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18d86fc28daff3c38218a28489384d34",
+    "url": "/goit-react-hw-02-phonebook/index.html"
+  },
+  {
+    "revision": "04aff7a6c28575e13e44",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.3324a89b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.3324a89b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dbd71995eb1eb6b13b3d",
+    "url": "/goit-react-hw-02-phonebook/static/js/main.9f508525.chunk.js"
+  },
+  {
+    "revision": "65c8a2f1bf8ffcc01bde",
+    "url": "/goit-react-hw-02-phonebook/static/js/runtime-main.d8bf734b.js"
+  }
+]);
